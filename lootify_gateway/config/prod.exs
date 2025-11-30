a829771c19a9424e30168462d8baa86546ceea5c
Dassis Miranda <dassishot@gmail.com>
2025-11-30 19:15:00 -0300

    Commit em 2025-11-30 07-15 PM

--- lootify_gateway/config/prod.exs
+++ lootify_gateway/config/prod.exs
@@ -1,8 +1,7 @@
 import Config
 
-# Force using SSL in production. This also sets the "strict-security-transport" header,
-# also known as HSTS. `:force_ssl` is required to be set at compile-time.
-config :lootify_gateway, LootifyGatewayWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+# Disable force SSL for now (enable when you have a proper SSL certificate)
+# config :lootify_gateway, LootifyGatewayWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
